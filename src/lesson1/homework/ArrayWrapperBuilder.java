cd553f0bdef37109ba74b870b0c6398c2103ff88
ololoiev <i.ololoiev@mail.ru>
2017-10-13 21:20:02 +0300

    учтены внесённые замечания

--- src/lesson1/homework/ArrayWrapperBuilder.java
+++ src/lesson1/homework/ArrayWrapperBuilder.java
@@ -1,12 +1,21 @@
 package lesson1.homework;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ArrayWrapperBuilder {
 
     public static final Integer MAX_PLENUM = 10;
     private int[] arr = new int[MAX_PLENUM];
     private int plenum = 0;
+    private BufferedReader bufferedReader;
 
-    public boolean addValue(int value){
+    public ArrayWrapperBuilder(BufferedReader bufferedReader) {
+        this.bufferedReader = bufferedReader;
+    }
+
+    private boolean addValue(int value){
         if(plenum < MAX_PLENUM) {
             if (checkValue(value)) {
                 arr[plenum] = value;
@@ -20,6 +29,40 @@ public class ArrayWrapperBuilder {
             System.out.println("Значений достаточно.");
             return false;
         }
+    }
+
+    public ArrayWrapper createArrayWrapper(){
+        System.out.println("Введите не больше 10 целых уникальных чисел, когда закончите введите пустую строку.");
+
+        ArrayWrapperBuilder builder = new ArrayWrapperBuilder(bufferedReader);
+        String value = getValue();
+        while (!value.equals("") &&
+                builder.getPlenum() < ArrayWrapperBuilder.MAX_PLENUM){
+            builder.addValue(Integer.parseInt(value));
+            value = getValue();
+        }
+
+        System.out.println("Ввод чисел закончен, спасибо.");
+
+        return builder.create();
+    }
+
+    private String getValue(){
+        String line;
+            while (true){
+                try {
+                    line = bufferedReader.readLine();
+                    if(line.equals("")){
+                        return line;
+                    }
+                    Integer.parseInt(line);
+                    return line;
+                }catch (IOException e){
+                    e.printStackTrace();
+                }catch (NumberFormatException e){
+                    System.out.println("Введиие целое число или пустую строку");
+                }
+            }
     }
 
     private boolean checkValue(int value){
--- src/lesson1/homework/Main.java
+++ src/lesson1/homework/Main.java
@@ -1,12 +1,16 @@
 package lesson1.homework;
 
+
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayWrapper arrayWrapper = createArrayWrapper();
+        BufferedReader bufferedReader = new BufferedReader(
+                new InputStreamReader(System.in));
+        ArrayWrapper arrayWrapper =
+                new ArrayWrapperBuilder(bufferedReader)
+                .createArrayWrapper();
 
         while (true){
             System.out.println();
@@ -17,40 +21,4 @@ public class Main {
 
     }
 
-    private static ArrayWrapper createArrayWrapper(){
-        System.out.println("Введите не больше 10 целых уникальных чисел, когда закончите введите пустую строку.");
-
-        ArrayWrapperBuilder builder = new ArrayWrapperBuilder();
-        String value = getValue();
-        while (!value.equals("") &&
-                builder.getPlenum() < builder.MAX_PLENUM){
-            builder.addValue(Integer.parseInt(value));
-            value = getValue();
-        }
-
-        System.out.println("Ввод чисел закончен, спасибо.");
-
-        return builder.create();
-    }
-
-
-
-    private static String getValue(){
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        String line;
-        while (true){
-            try {
-                line = bf.readLine();
-                if(line.equals("")){
-                    return line;
-                }
-                Integer.parseInt(line);
-                return line;
-            }catch (IOException e){
-                e.printStackTrace();
-            }catch (NumberFormatException e){
-                System.out.println("Введиие целое число или пустую строку");
-            }
-        }
-    }
 }
